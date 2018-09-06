5f3357a73e0dd99dc7b93913b3740d951990be0b
Ellis Crawford <ellicraw@gmail.com>
2018-09-06 12:48:16 -0400

    starting over

--- tumblr_feed/Pods/Target Support Files/Pods-tumblr_feed/Pods-tumblr_feed-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tumblr_feed : NSObject
-@end
-@implementation PodsDummy_Pods_tumblr_feed
-@end
--- tumblr_feed/Pods/Target Support Files/Pods-tumblr_feedTests/Pods-tumblr_feedTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tumblr_feedTests : NSObject
-@end
-@implementation PodsDummy_Pods_tumblr_feedTests
-@end
