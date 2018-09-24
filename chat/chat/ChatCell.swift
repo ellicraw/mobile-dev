b2c28645598e6c36702e8fde2a9a54249a8552b6
Ellis Crawford <ellicraw@gmail.com>
2018-09-23 22:40:19 -0400

    trying

--- chat/chat/ChatCell.swift
+++ chat/chat/ChatCell.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ChatCell: UITableViewCell {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
