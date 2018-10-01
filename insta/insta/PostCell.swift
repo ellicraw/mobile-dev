a3ac7ba800c3591efda5e74debd4941426edbb59
Ellis Crawford <ellicraw@gmail.com>
2018-10-01 12:26:46 -0400

    README

---
+++ insta/insta/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  insta
+//
+//  Created by Ellis Crawford on 10/1/18.
+//  Copyright © 2018 Ellis Crawford. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
